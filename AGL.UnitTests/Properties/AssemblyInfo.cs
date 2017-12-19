e538cf7367822d331978068dab318ad551e67026
VeritasSoftware <shantanu_negi@hotmail.com>
2017-12-19 19:18:31 +1100

    Inital commit

---
+++ AGL.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AGL.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("AGL.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3408588-2065-47c8-b20c-b30201e46602")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AGL/Program.cs
@@ -0,0 +1,65 @@
+﻿using AGL.Application;
+using AGL.Entities;
+using AGL.Repository;
+using Autofac;
+using System;
+using System.Linq;
+
+namespace AGL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var containerBuilder = new ContainerBuilder();
+
+                containerBuilder.RegisterType<PetsRepository>().As<IPetsRepository>().WithProperty("Url", Properties.Settings.Default.ServiceUrl);
+                containerBuilder.RegisterType<PetsManager>().As<IPetsManager>();
+
+                var container = containerBuilder.Build();                
+
+                using (IPetsManager petsManager = container.Resolve<IPetsManager>())
+                {                    
+                    //get cats by person gender
+                    var catsByPersonGenderCollection = petsManager.GetPetsByPersonGender(PetType.Cat);
+
+                    DisplayCatsByPersonGender(catsByPersonGenderCollection.Result);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error occured! " + ex.Message);
+            }
+
+            //Console.WriteLine("Please wait for results...");
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Display Cats by Person's gender
+        /// </summary>
+        /// <param name="petsByPersonGenderCollection">Cats by Owners gender list</param>
+        static void DisplayCatsByPersonGender(PetsByPersonGenderCollection petsByPersonGenderCollection)
+        {
+            var catsByOwnerGender = petsByPersonGenderCollection.PetsByPersonGender;
+
+            //Write output
+            Console.WriteLine(catsByOwnerGender.ElementAt(0).Gender);
+
+            catsByOwnerGender.ElementAt(0).Pets.Select(pet => pet.name).OrderBy(name => name).ToList().ForEach(catName =>
+            {
+                Console.WriteLine("\t" + catName);
+            });
+
+            Console.WriteLine(catsByOwnerGender.ElementAt(1).Gender);
+
+            catsByOwnerGender.ElementAt(1).Pets.Select(pet => pet.name).OrderBy(name => name).ToList().ForEach(catName =>
+            {
+                Console.WriteLine("\t" + catName);
+            });
+        }
+    }
+}
